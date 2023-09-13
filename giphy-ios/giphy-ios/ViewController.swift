967dd3d7f3fb460ab27a74cf73eae623db90c25a
Davis Zarins <davis.zars@gmail.com>
2023-09-13 19:43:51 +0300

    empty project

---
+++ giphy-ios/giphy-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  giphy-ios
+//
+//  Created by DavisZ on 13/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
